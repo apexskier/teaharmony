3d806cfc981eddd42b2f64d4391677f4b881a4e7
Cameron Little <cameron@camlittle.com>
2013-11-24 21:34:49 +0000

    Playing with some page and message animation when clicking links

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,27 @@
-$('#next').click(function(e) {
-    e.preventDefault();
-    $('.page-1').fadeOut(200, function() {
-        $('.page-2').fadeIn(200);
-        $('input[name="email"]').focus();
+$(document).ready(function() {
+    $('#next').click(function(e) {
+        e.preventDefault();
+        $('.page-1').fadeOut(200, function() {
+            $('.page-2').fadeIn(200);
+            $('input[name="email"]').focus();
+        });
     });
-});
-$('#back').click(function(e) {
-    e.preventDefault();
-    $('.page-2').fadeOut(200, function() {
-        $('.page-1').fadeIn(200);
-        $('input[name="username"]').focus();
+    $('#back').click(function(e) {
+        e.preventDefault();
+        $('.page-2').fadeOut(200, function() {
+            $('.page-1').fadeIn(200);
+            $('input[name="username"]').focus();
+        });
     });
-});
-$('form').on('submit', function(e) {
-    $(this).find('button[type="submit"]').prop('disabled', true);
+    $('form').on('submit', function(e) {
+        $(this).find('button[type="submit"]').prop('disabled', true);
+        $('#content').addClass('move-out');
+        $('.alert').addClass('message-leave');
+    });
+    $('#content').removeClass('just-loaded');
+    $('.alert').removeClass('message-out');
+    $('a').click(function(e) {
+        $('#content').addClass('move-out');
+        $('.alert').addClass('message-leave');
+    });
 });
